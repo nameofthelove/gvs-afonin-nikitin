32f71f2cccc0ed64f397acc19d63176664eceae4
vldxdd <ilya2005a@mail.ru>
2026-09-14 06:05:11 +0700

    обновил vector.cuh вроде с добавлением переполнения

--- core/include/vector.cuh
+++ core/include/vector.cuh
@@ -1,19 +1,39 @@
 #pragma once
 #include "data.cuh"
 #include "vector_view.cuh"
+#include "kernel.cuh"
 #include <memory>
 #include <cstddef>
+#include <stdexcept>
 
-template <typename T>
+template <typename AtomT>
 class Vector {
 private:
-	shared_ptr<Data<AtomT>> data_;
-	VectorView<AtomT> view_;
+    std::shared_ptr<Data<AtomT>> data_;
+    VectorView<AtomT> view_;
+
 public:
-	explicit Vector(std::size_t size) : data_(std::make_shared<Data<AtomT>>(size)), view_(data_->data(), size) {}
-	size_t size() const { return *view_.size(); }
-	Data<AtomT> data() { return &data_}
-	const Data<AtomT> data() const { return &data_ }
-	VectorView<AtomT>& view() { return view_ }
-	const VectorView<AtomT>& view() const { return view_ }
+    explicit Vector(std::size_t size) 
+        : data_(std::make_shared<Data<AtomT>>(size)), view_(data_->data(), size) {}
+
+    size_t size() const { return view_.size(); }
+    VectorView<AtomT>& view() { return view_; }
+    const VectorView<AtomT>& view() const { return view_; }
+
+    Vector operator+(const Vector& other) const {
+        if (this->size() != other.size()) {
+            throw std::invalid_argument("Vector dimensions must match for addition");
+        }
+
+        Vector result(this->size());
+
+        int threadsPerBlock = 256;
+        int blocksPerGrid = (this->size() + threadsPerBlock - 1) / threadsPerBlock;
+
+        // Вызов кёрнела с передачей VectorView по значению
+        kernel_vecadd<<<blocksPerGrid, threadsPerBlock>>>(this->view_, other.view_, result.view_);
+        cudaDeviceSynchronize();
+
+        return result;
+    }
 };
