ac1a9af9129fb2646ca2f362f12bd50bb10dee86
vldxdd <ilya2005a@mail.ru>
2026-09-14 06:00:58 +0700

    изменённый vector_view с добавлением геттера size

--- core/include/vector_view.cuh
+++ core/include/vector_view.cuh
@@ -13,4 +13,6 @@ public:
 	__host__ __device__ const AtomT& operator[](std::size_t n) const { return data_[n]; }
 	__host__ __device__ AtomT& operator()(std::size_t i) { return data_[i]; }
 	__host__ __device__ const AtomT& operator()(std::size_t i) const { return data_[i]; }
+	__host__ __device__ size_t size() const { return size_; }
 };
+
